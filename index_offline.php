77c13b463b2597693ae9e3003a584c2a808430f6
Adam Franco <afranco@middlebury.edu>
2002-11-14 16:03:46 +0000

    Replace index.php with this file when doing major upgrades.
    It says:
    SitesDB is offline for upgrades, please return in a few minutes.

---
+++ index_offline.php
@@ -0,0 +1,15 @@
+<!-- index.html for temporarily offline -->
+
+<html>
+<head>
+<title>SitesDB</title>
+</head>
+<body>
+<center>
+<h1>SitesDB</h1>
+<h2>SitesDB is temporarilly offline for upgrades.<br>Please check back in a few minutes.</h2>
+
+
+</center>
+</body>
+</html>
