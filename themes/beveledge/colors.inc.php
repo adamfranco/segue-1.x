ce197badd640bdf09a99d1367bbbd35e808d7c04
Adam Franco <afranco@middlebury.edu>
2002-12-16 16:54:23 +0000

    new themes from alex

---
+++ themes/beveledge/colors.inc.php
@@ -0,0 +1,175 @@
+<? // theme defaults file
+	// this file contains defaults for text color, hiliting, etc etc etc
+	
+$_theme_colors = array(
+	"white"=>array(
+			"a"=>					"966",
+			"a:hover"=>				"b55",
+			"font-size"=>			"12px",
+			"input-borders"=>		"555",
+			"input-size"=>			"12px",
+			"box-color"=>			"efefef",
+			"box-border-color"=>	"999",
+			"btnlink-color"=>		"d33",
+			"btnlink-border-color"=>"d55",
+			"title-color"=>			"aaa",
+			"title-under-color"=>	"aaa",
+			"th-color"=>			"777",
+			"th-background"=>		"e9e9e9",
+			"td-color"=>			"333",
+			"td0"=>					"e6e6ff",
+			"td1"=>					"f6f6ff",
+			"header"=>				"fff",
+			"topnav"=>				"fff",
+			"contentarea"=>			"fff",
+			"contentinfo"=>			"999"
+	),
+	"yellow"=>array(
+			"a"=>					"624",
+			"a:hover"=>				"024",
+			"font-size"=>			"12px",
+			"input-borders"=>		"555",
+			"input-size"=>			"12px",
+			"box-color"=>			"efefef",
+			"box-border-color"=>	"999",
+			"btnlink-color"=>		"a33",
+			"btnlink-border-color"=>"b55",
+			"title-color"=>			"aaa",
+			"title-under-color"=>	"aaa",
+			"th-color"=>			"777",
+			"th-background"=>		"e9e9e9",
+			"td-color"=>			"333",
+			"td0"=>					"e6e6ff",
+			"td1"=>					"f6f6ff",
+			"header"=>				"A1AEBE",
+			"topnav"=>				"909DAD",
+			"contentarea"=>			"FFFFCC",
+			"contentinfo"=>			"999"
+	),
+		"lightblue"=>array(
+			"a"=>					"624",
+			"a:hover"=>				"024",
+			"font-size"=>			"12px",
+			"input-borders"=>		"555",
+			"input-size"=>			"12px",
+			"box-color"=>			"efefef",
+			"box-border-color"=>	"999",
+			"btnlink-color"=>		"a33",
+			"btnlink-border-color"=>"b55",
+			"title-color"=>			"aaa",
+			"title-under-color"=>	"aaa",
+			"th-color"=>			"777",
+			"th-background"=>		"e9e9e9",
+			"td-color"=>			"333",
+			"td0"=>					"e6e6ff",
+			"td1"=>					"f6f6ff",
+			"header"=>				"A1AEBE",
+			"topnav"=>				"909DAD",
+			"contentarea"=>			"ebeFf5",
+			"contentinfo"=>			"999"
+	),
+	"olive"=>array(
+			"a"=>					"026",
+			"a:hover"=>				"060",
+			"font-size"=>			"12px",
+			"input-borders"=>		"555",
+			"input-size"=>			"12px",
+			"box-color"=>			"efefef",
+			"box-border-color"=>	"999",
+			"btnlink-color"=>		"a33",
+			"btnlink-border-color"=>"b55",
+			"title-color"=>			"888",
+			"title-under-color"=>	"888",
+			"th-color"=>			"777",
+			"th-background"=>		"e9e9e9",
+			"td-color"=>			"333",
+			"td0"=>					"e6e6ff",
+			"td1"=>					"f6f6ff",
+			"header"=>				"898",
+			"topnav"=>				"aba",
+			"contentarea"=>			"bcb",
+			"contentinfo"=>			"999"
+	),
+	"red"=>array(
+			"a"=>					"620",
+			"a:hover"=>				"048",
+			"text"=>				"444",
+			"font-size"=>			"12px",
+			"input-borders"=>		"555",
+			"input-size"=>			"12px",
+			"box-color"=>			"efefef",
+			"box-border-color"=>	"999",
+			"btnlink-color"=>		"a33",
+			"btnlink-border-color"=>"b55",
+			"title-color"=>			"666",
+			"title-under-color"=>	"888",
+			"th-color"=>			"777",
+			"th-background"=>		"e9e9e9",
+			"td-color"=>			"333",
+			"td0"=>					"e6e6ff",
+			"td1"=>					"f6f6ff",
+			"header"=>				"b88",
+			"topnav"=>				"c99",
+			"contentarea"=>			"edd",
+			"contentinfo"=>			"999"
+	)
+);
+
+$_borderstyle = array(
+	"dashed"=>					"dashed",
+	"solid"=>					"solid",
+	"dotted"=>					"dotted"
+);
+
+$_bordercolor = array(
+	"black"=>					"000000",
+	"red"=>						"990000",
+	"blue"=>					"000033"
+);
+
+$_bgcolor = array(
+	"blue"=>array(
+			"bgshadow"=>		"blue",
+			"bg"=>				"5F7082"					
+	),
+	"gray"=>array(
+			"bgshadow"=>		"gray",
+			"bg"=>				"999999"					
+	),
+	"white"=>array(
+			"bgshadow"=>		"white",
+			"bg"=>				"FFFFFF"					
+	),	
+	"yellow"=>array(
+			"bgshadow"=>		"yellow",
+			"bg"=>				"FFFFCC"					
+	)	
+
+);
+
+$_textcolor = array(
+	"black"=>					"444",
+	"red"=>						"990000",
+	"blue"=>					"000033"
+);
+
+$_linkcolor = array(
+	"red"=>						"990000",
+	"blue"=>					"003366",
+	"green"=>					"006633"
+);
+
+$_nav_arrange = array(
+	"Top Sections"=>			"1",
+	"Side Sections"=>			"2"
+);
+
+$_nav_width = array(
+	"150 pixels"=>				"150",
+	"175 pixels"=>				"175",
+	"200 pixels"=>				"200",
+	"250 pixels"=>				"250",
+	"275 pixels"=>				"275"
+);
+
+
---
+++ themes/beveledge/output.inc.php
@@ -0,0 +1,312 @@
+<? // output.inc.php
+	// this script outputs the HTML resulting from action files
+	
+/* -------------- THEME SETTINGS ---------------------	*/
+/*		handle the $themesettings array					*/
+//print "$themesdir/$theme";
+if (file_exists("$themesdir/$theme/colors.inc.php"))
+	include("$themesdir/$theme/colors.inc.php");
+	
+//$nav_arrange=2;
+
+if ($themesettings[theme] == 'beveledge') {   // indeed these settings are for this theme
+
+	$usebg = $themesettings[bgcolor];
+	$usecolor = $themesettings[colorscheme];
+	$useborder = $themesettings[borderstyle];
+	$usebordercolor = $themesettings[bordercolor];
+	$usetextcolor = $themesettings[textcolor];
+	$uselinkcolor = $themesettings[linkcolor];
+	$usenav = $themesettings[nav_arrange];
+	$usenavwidth = $themesettings[nav_width];
+	
+}
+if (!$usebg) $usebg = 'blue';
+$bg = $_bgcolor[$usebg];
+
+if (!$usecolor) $usecolor = 'white';
+$c = $_theme_colors[$usecolor];
+
+if (!$useborder) $useborder = 'solid';
+$borders = $_borderstyle[$useborder];
+
+if (!$usebordercolor) $usebordercolor = 'blue';
+$bordercolor = $_bordercolor[$usebordercolor];
+
+if (!$usetextcolor) $usetextcolor = 'black';
+$textcolor = $_textcolor[$usetextcolor];
+
+if (!$uselinkcolor) $uselinkcolor = 'red';
+$linkcolor = $_linkcolor[$uselinkcolor];
+
+if (!$usenav) $usenav = 'Top Sections';
+$nav_arrange = $_nav_arrange[$usenav];
+
+if (!$usenavwidth) $usenavwidth = '150 pixels';
+$navwidth = $_nav_width[$usenavwidth];
+
+
+/* ------------------- END ---------------------------	*/
+
+?>
+<head>
+<?
+/* ------------------------------------------- */
+/* ------------- COMMON HEADER --------------- */
+/* ------------------------------------------- */
+include("themes/common/header.inc.php"); ?>
+
+
+<? include("themes/$theme/css.inc.php"); ?>
+
+<?
+/* ------------------------------------------- */
+/* -------------- PAGE TITLE ----------------- */
+/* ------------------------------------------- */
+?>
+<title><? echo $pagetitle; ?></title>
+
+</head>
+
+<body marginheight=0 marginwidth=0 leftmargin=0 topmargin=0 rightmargin=0>
+
+<!--<tr>
+<td class=header align=center> -->
+<?
+/* ------------------------------------------- */
+/* -------------- STATUS BAR ----------------- */
+/* ------------------------------------------- */
+//include("themes/common/status.inc.php"); 
+?>
+<!-- </td>
+</tr>
+</table>-->
+
+<table width=100% cellpadding=0 cellspacing=0>
+<tr>
+<td class=topleft>&nbsp;</td>
+<td class=top>&nbsp;</td>
+<td class=topright>&nbsp;</td>
+</tr>
+<tr>
+<td class=left><img class=lefttop src='<? echo "$themesdir/$theme/images/$bg[bgshadow]/lefttop.gif"?>'></td>
+<td class=content>
+
+<div class=header>
+<?
+/* ------------------------------------------- */
+/* ------------- SITE HEADER ----------------- */
+/* ------------------------------------------- */
+print $siteheader; 
+/* ------------------------------------------- */
+/* -------------- STATUS BAR ----------------- */
+/* ------------------------------------------- */
+
+include("themes/common/status.inc.php"); 
+print $sitecrumbs;
+?>
+</div>
+<div class=topnav align=center>
+<?
+//use this if Section navigation is in left nav
+if ($nav_arrange==1) {
+	/* ------------------------------------------- */
+	/* -------------- TOP NAV-sections ------------ */
+	/* ------------------------------------------- */
+	//print " | ";
+	$totalnav = count($topnav);
+	$next=1;
+	foreach ($topnav as $item) {
+		$samepage = (isset($section) && ($section == $item[id]))?1:0;
+		if (!$section) $samepage = ($action && ($action == $item[id]))?1:0;
+		print makelink($item,$samepage);
+		if ($next != $totalnav) print " | ";
+		$next=$next+1;
+	}	
+	print $topnav_extra;
+}
+?>
+</div>
+
+<table width=100% class=contenttable>
+<tr>
+<td class=leftnav>
+<table width=100% cellpadding=2 cellspacing=0>	
+<?
+//use this if page navigation is in left nav
+if ($nav_arrange==1) {
+	/* ------------------------------------------- */
+	/* -------------- LEFT NAV   ----------------- */
+	/* ------------------------------------------- */
+		
+	foreach ($leftnav as $item) {
+		print "<tr><td>";
+		if ($item[type] == 'normal') {
+			$samepage = (isset($page) && ($page == $item[id]))?1:0;
+			if (!$page) $samepage = ($action && ($action == $item[id]))?1:0;
+			print "<div>";
+			print makelink($item,$samepage,'',1);
+			print "</div>";
+		}
+		if ($item[type] == 'divider') {
+			print "$item[extra]<br>";
+		}
+		if ($item[type] == 'heading') {
+			print "<img src='$themesdir/breadloaf/images/bullet.gif' border=0 align=absmiddle> $item[name] :";
+			if ($item[extra]) print "<div align=right>$item[extra]</div>";
+		}
+		print "</tr></td>";
+	}
+	print "</table>";
+	print "<br>$leftnav_extra";
+}
+//use this if Section navigation is in left nav
+if ($nav_arrange==2) {
+
+/* ------------------------------------------- */
+/* -------------- LEFT NAV-sections ---------- */
+/* ------------------------------------------- */
+		
+	foreach ($topnav as $item) {
+	print "<tr><td>";
+		if ($item[type] == 'normal') {
+			$samepage = (isset($section) && ($section == $item[id]))?1:0;
+			//$samepage = (isset($page) && ($page == $item[id]))?1:0;
+			if (!$page) $samepage = ($action && ($action == $item[id]))?1:0;
+			print "<div>";
+			print makelink($item,$samepage,'',1);
+			print "</div>";
+		}
+		if ($item[type] == 'divider') {
+			print "$item[extra]<br>";
+		}
+		if ($item[type] == 'heading') {
+			print "<img src='$themesdir/breadloaf/images/bullet.gif' border=0 align=absmiddle> $item[name] :";
+			if ($item[extra]) print "<div align=right>$item[extra]</div>";
+		}
+		print "</tr></td>";
+	}
+	print "</table>";
+	print "<br>$topnav_extra";
+	//print "<br>$leftnav_extra";
+}
+?>
+</td>
+<td class=contentarea>
+<div class=topnav align=center>
+<?
+//use this if page navigation on top
+if ($nav_arrange==2) {
+	/* ------------------------------------------- */
+	/* -------------- TOP NAV-pages -------------- */
+	/* ------------------------------------------- */
+	//print " | ";
+	$totalnav = count($leftnav);
+	$next=1;
+	foreach ($leftnav as $item) {
+		$samepage = (isset($page) && ($page == $item[id]))?1:0;
+		//$samepage = (isset($section) && ($section == $item[id]))?1:0;
+		if (!$section) $samepage = ($action && ($action == $item[id]))?1:0;
+		print makelink($item,$samepage);
+		if ($next != $totalnav) print " | ";
+		$next=$next+1;
+	}
+	print $leftnav_extra;
+	//print $topnav_extra;
+}
+?>
+</div>
+<?
+/* ------------------------------------------- */
+/* -------------- CONTENT AREA   ------------- */
+/* ------------------------------------------- */
+
+print $content; 
+
+?>
+<div class=topnav align=center>
+<?
+//use this if page navigation on top and bottom
+if ($nav_arrange==2) {
+	/* ------------------------------------------- */
+	/* -------------- Bottom NAV-pages -------------- */
+	/* ------------------------------------------- */
+	//print " | ";
+	$totalnav = count($leftnav);
+	$next=1;
+	foreach ($leftnav as $item) {
+		$samepage = (isset($page) && ($page == $item[id]))?1:0;
+		//$samepage = (isset($section) && ($section == $item[id]))?1:0;
+		if (!$section) $samepage = ($action && ($action == $item[id]))?1:0;
+		print makelink($item,$samepage);
+		if ($next != $totalnav) print " | ";
+		$next=$next+1;
+	}
+	print $leftnav_extra;
+	//print $topnav_extra;
+}
+ 
+?>
+</div>
+</td>
+<?
+/* ------------------------------------------- */
+/* -------------- RIGHT NAV      ------------- */
+/* ------------------------------------------- */
+
+if (count($rightnav)) {
+	print "<td style='margin-left: 20px'>";
+	foreach ($rightnav as $item) {
+		print "<a href='$item[url]'>$item[name]</a><BR>";
+	}
+	print "</td>";
+}
+?>
+</tr>
+</table>
+<?
+
+?>
+<div class=topnav align=center>
+<?
+//use this if Section navigation is top an bottom
+
+/* ------------------------------------------- */
+/* --------- bottom NAV -sections --- */
+/* ------------------------------------------- */
+if ($nav_arrange==1) {
+	//print " | ";
+	$totalnav = count($topnav);
+	$next=1;
+	foreach ($topnav as $item) {
+		$samepage = (isset($section) && ($section == $item[id]))?1:0;
+		if (!$section) $samepage = ($action && ($action == $item[id]))?1:0;
+		$item[extra] = "";
+		print makelink($item,$samepage);
+		if ($next != $totalnav) print " | ";
+		$next=$next+1;
+	}
+}
+
+?>
+</div>
+
+<?
+/* ------------------------------------------- */
+/* -------------- FOOTER     ----------------- */
+/* ------------------------------------------- */
+print $sitefooter ?>
+
+</td> <!-- end content table cell -->
+<td class=right><img class=righttop src='<? echo "$themesdir/$theme/images/$bg[bgshadow]/righttop.gif"?>'></td>
+</tr>
+<tr>
+<td class=bottomleft>&nbsp;</td>
+<td class=bottom>&nbsp;</td>
+<td class=bottomright>&nbsp;</td>
+
+</tr>
+
+</table>
+
+	
