0dd70e5a51728048a912decd72da35cf08b9dac2
Adam Franco <afranco@middlebury.edu>
2002-12-16 16:45:07 +0000

    new theme

---
+++ themes/tornpieces/colors.inc.php
@@ -0,0 +1,84 @@
+<? // theme defaults file
+	// this file contains defaults for text color, hiliting, etc etc etc
+	
+$_theme_colors = array(
+	"white"=>array(
+			"a"=>					"966",
+			"a:hover"=>				"b55",
+			"font-size"=>			"12px",
+			"input-borders"=>		"555",
+			"input-size"=>			"12px",
+			"box-color"=>			"efefef",
+			"box-border-color"=>	"999",
+			"btnlink-color"=>		"d33",
+			"btnlink-border-color"=>"d55",
+			"title-color"=>			"aaa",
+			"title-under-color"=>	"aaa",
+			"th-color"=>			"777",
+			"th-background"=>		"e9e9e9",
+			"td-color"=>			"333",
+			"td0"=>					"e6e6ff",
+			"td1"=>					"f6f6ff",
+			"header"=>				"fff",
+			"topnav"=>				"fff",
+			"contentarea"=>			"fff",
+			"contentinfo"=>			"999"
+	)
+);
+
+$_borderstyle = array(
+	"dashed"=>					"dashed",
+	"solid"=>					"solid",
+	"dotted"=>					"dotted"
+);
+
+$_bordercolor = array(
+	"black"=>					"000000",
+	"red"=>						"990000",
+	"blue"=>					"000033"
+);
+
+$_bgcolor = array(
+	"white"=>array(
+			"bgshadow"=>		"white",
+			"bg"=>				"FFFFFF"					
+	),
+	"gray"=>array(
+			"bgshadow"=>		"gray",
+			"bg"=>				"CCCCCC"					
+	),
+	"yellow"=>array(
+			"bgshadow"=>		"yellow",
+			"bg"=>				"FFFFCC"					
+	),
+	"blue"=>array(
+			"bgshadow"=>		"blue",
+			"bg"=>				"5F7082"					
+	)	
+);
+
+$_textcolor = array(
+	"black"=>					"444",
+	"red"=>						"990000",
+	"blue"=>					"000033"
+);
+
+$_linkcolor = array(
+	"red"=>						"990000",
+	"blue"=>					"003366",
+	"green"=>					"006633"
+);
+
+$_nav_arrange = array(
+	"Top Sections"=>			"1",
+	"Side Sections"=>			"2"
+);
+
+$_nav_width = array(
+	"150 pixels"=>				"150",
+	"175 pixels"=>				"175",
+	"200 pixels"=>				"200",
+	"250 pixels"=>				"250",
+	"275 pixels"=>				"275"
+);
+
