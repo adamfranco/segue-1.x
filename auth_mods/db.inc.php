8727ebb0f2e2cb11ba44cbb855b4a7673d7a062f
Adam Franco <afranco@middlebury.edu>
2003-03-27 22:08:41 +0000

    adding in dobo's changes to keep from segue1-2

--- auth_mods/db.inc.php
+++ auth_mods/db.inc.php
@@ -4,10 +4,7 @@ function _valid_db($name,$pass,$admin_auser=0) {
 	$name = strtolower($name);
 	global $dbhost, $dbuser,$dbpass, $dbdb;
 	db_connect($dbhost,$dbuser,$dbpass,$dbdb);
-	$query = "select * from users where uname='$name'".(($admin_auser)?"":" and pass='$pass' and status='db'");
-//	print $query; // debug
-//	$query = "select * from users where uname='$name'and pass='$pass' and status!='ldap'";
-//	$query = "select * from users where uname='$name'";
+	$query = "SELECT * FROM user WHERE user_uname='$name'".(($admin_auser)?"":" AND user_pass='$pass' AND user_authtype='db'");
 	$r = db_query($query);
 //	$a = db_fetch_assoc($r);
 	
@@ -15,13 +12,13 @@ function _valid_db($name,$pass,$admin_auser=0) {
 	if (db_num_rows($r)) {
 		$a = db_fetch_assoc($r);
 		$x = array(); // array for returned info
-		$x[fullname] = $a[fname];
+		$x[fullname] = $a[user_fname];
 		$x[user] = $name;
 		$x[pass] = $pass;
-		$x[email] = $a[email];
-		$x[type] = $a[type];
+		$x[email] = $a[user_email];
+		$x[type] = $a[user_type];
 		$x[method] = 'db';
-		$x[id] = $a[id];
+		$x[id] = $a[user_id];
 		return $x;
 	} /*else {
 	    $query = "select * from users where email='$name' and pass='$pass' and status='open'";
