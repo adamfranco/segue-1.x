e22465487411ab30ba71f3589dedad2aa22b140d
Gabe Schine <gabe@shine.net>
2003-06-03 14:34:44 +0000

    added ugroup object

---
+++ objects/ugroup.inc.php
@@ -0,0 +1,27 @@
+<? /* $Id$ */
+
+class ugroup {
+	var $id;
+/******************************************************************************
+ * this class is temporary -- it's a quickie fix
+ *
+ * 
+ ******************************************************************************/
+	function getGroupID($name) {
+		$query = "
+	SELECT
+		ugroup_id
+	FROM
+		ugroup
+	WHERE
+		ugroup_name='$name'
+	";
+		$r = db_query($query);
+		if (db_num_rows($r)) {
+			$a = db_fetch_assoc($r);
+			return $a['ugroup_id'];
+		}
+		return false;
+	}
+		
+}
