f130f175890cecd530c4e1c01375db6dc0597b49
Adam Franco <afranco@middlebury.edu>
2004-02-16 23:34:15 +0000

    now runs as a command-line script.

--- export/domit_site_export.php
+++ export/domit_site_export.php
@@ -18,54 +18,68 @@ require_once('../objects/page.inc.php');
 require_once('../objects/story.inc.php');
 require_once('../permissions.inc.php');
 
-// temporary configs
-$sitename = 'afranco';
-$exportpath = '/www/afranco/segue_backup/';
-$compressCommand = 'tar -czf ';
-$compressOrder = 'destFirst'; // 'destFirst' or 'destLast'
+// print help if requested/needed
+if ($argc != 3 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
 
-// Fetch the site.
-$site =& new Site($sitename);
-$site->fetchDown(TRUE);
-$site->fetchSiteAtOnceForeverAndEverAndDontForgetThePermissionsAsWell_Amen();
+?>
 
-// Fetch the location of the media files.
-$imagepath = $uploaddir.$sitename.'/';
+This script will export the specified Segue site 
+and its media to the specified directory.
 
-// Get the XML for the site
-$siteExporter =& new DomitSiteExporter();
-$siteXML =& $siteExporter->export($site);
+Usage:
 
-//print_r($siteExporter);
+<?php echo $argv[0]; ?> <site name> <output directory>
 
-// make a temporary directory for the site contents
-if (file_exists($exportpath.'tmp'))
-	deletePath($exportpath.'tmp');
-mkdir ($exportpath.'tmp');
-//$siteDir = $exportpath.'tmp/'.$sitename.'_'.date('YmdHis').'/';
-$siteDir = $exportpath.'tmp/'.$sitename.'/';
-mkdir ($siteDir);
+With the --help, -help, -h,
+or -? options, you can get this help.
 
-// Copy the Media Files to the sitedir.
-dir_copy( $imagepath, $siteDir.'media/');
+<?
+} else {
+	
+	$sitename = $argv[1];
+	$exportpath = $argv[2];
 
-// Save the XML to a file.
-$xmlFile = $siteDir.'site.xml';
+	// Fetch the site.
+	$site =& new Site($sitename);
+	$site->fetchDown(TRUE);
+	$site->fetchSiteAtOnceForeverAndEverAndDontForgetThePermissionsAsWell_Amen();
+	
+	// Fetch the location of the media files.
+	$imagepath = $uploaddir.$sitename.'/';
+	
+	
+	
+	// Get the XML for the site
+	$siteExporter =& new DomitSiteExporter();
+	$siteXML =& $siteExporter->export($site);
+	
+	
+	
+	// make a directory for the site contents
+	$siteDir = $exportpath.$sitename.'/';
+	if (file_exists($siteDir))
+		deletePath($siteDir);
+	mkdir ($siteDir);
+	
+	// Copy the Media Files to the sitedir.
+	dir_copy( $imagepath, $siteDir.'media/');
+	
+	// Save the XML to a file.
+	$xmlFile = $siteDir.'site.xml';
+	
+	
+	if (!$handle = fopen($xmlFile, 'a')) {
+		 echo "Cannot open file ($xmlFile)";
+		 exit;
+	}
+	
+	// Write $somecontent to our opened file.
+	if (!fwrite($handle, $siteXML)) {
+	   echo "Cannot write to file ($xmlFile)";
+	   exit;
+	}
+	
+	fclose($handle);
 
-
-if (!$handle = fopen($xmlFile, 'a')) {
-	 echo "Cannot open file ($xmlFile)";
-	 exit;
 }
-
-// Write $somecontent to our opened file.
-if (!fwrite($handle, $siteXML)) {
-   echo "Cannot write to file ($xmlFile)";
-   exit;
-}
-
-fclose($handle);
-//$siteExporter->saveXML($xmlFile);
-//print $siteXML;
-
 exit(0);
