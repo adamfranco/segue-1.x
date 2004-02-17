80a876fcd54f6ded2be7ba308d85a201e4c7bcf6
Adam Franco <afranco@middlebury.edu>
2004-02-17 20:37:32 +0000

    now takes a verbos parameter and doesn't have a memory explosion.

--- export/export_all_sites.php
+++ export/export_all_sites.php
@@ -19,7 +19,7 @@ require_once('../objects/story.inc.php');
 require_once('../permissions.inc.php');
 
 // print help if requested/needed
-if ($argc != 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
+if ($argc < 2 || $argc > 3 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
 
 ?>
 
@@ -28,7 +28,11 @@ and their media to the specified directory.
 
 Usage:
 
-<?php echo $argv[0]; ?> <output directory>
+<?php echo $argv[0]; ?> [-v] <output directory>
+
+Options:
+	-v	Verbose output. Will print out the number 
+		and name of the sites being export.
 
 With the --help, -help, -h,
 or -? options, you can get this help.
@@ -36,7 +40,13 @@ or -? options, you can get this help.
 <?
 } else {
 	
-	$exportpath = $argv[1];
+	if ($argv[1] == '-v')
+		$verbose = TRUE;
+	else 
+		$verbose = FALSE;
+		
+	
+	$exportpath = $argv[$argc-1];
 	
 	db_connect($dbhost, $dbuser, $dbpass, $dbdb);
 	$query = "
@@ -56,48 +66,56 @@ or -? options, you can get this help.
 	while ($a = db_fetch_assoc($r)) {
 	
 		$sitename = $a['name'];
-		print "\n".$i."\t".$sitename."\t";
+		if ($verbose)
+			print "\n".$i."\t".$sitename."\t";
+		
 		$i++;
-	
-		// Fetch the site.
-		$site =& new Site($sitename);
-		$site->fetchDown(TRUE);
-		$site->fetchSiteAtOnceForeverAndEverAndDontForgetThePermissionsAsWell_Amen();
 		
-		// Fetch the location of the media files.
-		$imagepath = $uploaddir.$sitename.'/';
-		
-		// Get the XML for the site
-		$siteExporter =& new DomitSiteExporter();
-		$siteXML =& $siteExporter->export($site);
-		
-		
-		
-		// make a directory for the site contents
-		$siteDir = $exportpath.$sitename.'/';
-		if (file_exists($siteDir))
-			deletePath($siteDir);
-		mkdir ($siteDir);
-		
-		// Copy the Media Files to the sitedir.
-		dir_copy( $imagepath, $siteDir.'media/');
-		
-		// Save the XML to a file.
-		$xmlFile = $siteDir.'site.xml';
-		
-		
-		if (!$handle = fopen($xmlFile, 'a')) {
-			 echo "Cannot open file ($xmlFile)";
-			 exit;
-		}
-		
-		// Write $somecontent to our opened file.
-		if (!fwrite($handle, $siteXML)) {
-		   echo "Cannot write to file ($xmlFile)";
-		   exit;
-		}
-		
-		fclose($handle);
+		shell_exec("php domit_site_export.php ".$sitename." ".$exportpath);
+// 
+// 		// Fetch the site.
+// 		$site =& new Site($sitename);
+// 		$site->fetchDown(TRUE);
+// 		$site->fetchSiteAtOnceForeverAndEverAndDontForgetThePermissionsAsWell_Amen();
+// 		
+// 		// Fetch the location of the media files.
+// 		$imagepath = $uploaddir.$sitename.'/';
+// 		
+// 		// Get the XML for the site
+// 		$siteExporter =& new DomitSiteExporter();
+// 		$siteXML =& $siteExporter->export($site);
+// 		
+// 		
+// 		
+// 		// make a directory for the site contents
+// 		$siteDir = $exportpath.$sitename.'/';
+// 		if (file_exists($siteDir))
+// 			deletePath($siteDir);
+// 		mkdir ($siteDir);
+// 		
+// 		// Copy the Media Files to the sitedir.
+// 		dir_copy( $imagepath, $siteDir.'media/');
+// 		
+// 		// Save the XML to a file.
+// 		$xmlFile = $siteDir.'site.xml';
+// 		
+// 		
+// 		if (!$handle = fopen($xmlFile, 'a')) {
+// 			 echo "Cannot open file ($xmlFile)";
+// 			 exit;
+// 		}
+// 		
+// 		// Write $somecontent to our opened file.
+// 		if (!fwrite($handle, $siteXML)) {
+// 		   echo "Cannot write to file ($xmlFile)";
+// 		   exit;
+// 		}
+// 		
+// 		fclose($handle);
+// 		
+// 		unset($site, $siteExporter, $siteXML, 
+// 			$handle, $xmlFile, $siteDir, $imagepath, 
+// 			$sitename, $a);
 	}
 
 }
