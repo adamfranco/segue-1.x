78718f4851bb35c65b19616aa02f0d8985c3aab2
Adam Franco <afranco@middlebury.edu>
2004-02-26 20:54:40 +0000

    adding line returns for more sane output.

--- permissions.inc.php
+++ permissions.inc.php
@@ -41,8 +41,8 @@ class permissions {
 			$o->delEditor($_REQUEST[edname]);
 		}
 		
-		printc("<input type=hidden name=edaction value=''>");
-		printc("<input type=hidden name=edname value=''>");
+		printc("\n<input type=hidden name=edaction value=''>");
+		printc("\n<input type=hidden name=edname value=''>");
 		
 		if (isclass($sitename)) {
 			print "<script lang='javascript'>";
@@ -57,14 +57,14 @@ class permissions {
 		
 		$a = array(0=>4,1=>1);
 		
-		printc("<style type='text/css'>th, .td0, .td1 {font-size: 10px;}</style>");
-		printc("<table width=100% style='border: 1px solid gray'>");
-		printc("<tr><th width=50%>name</th>	<th colspan=".($a[$d])." width=30%>permissions</th>");
+		printc("\n<style type='text/css'>th, .td0, .td1 {font-size: 10px;}</style>");
+		printc("\n<table width=100% style='border: 1px solid gray'>");
+		printc("\n<tr><th width=50%>name</th>	<th colspan=".($a[$d])." width=30%>permissions</th>");
 		if ($canAddEditors) {
 			printc("<th>del</th>");
 		}
 		printc("</tr>");
-		printc("<tr><th>&nbsp;</th>".(($d)?"<th>discuss</th>":"<th>add</th><th>edit</th><th>delete</th><th>view</th>"));
+		printc("\n<tr><th>&nbsp;</th>".(($d)?"<th>discuss</th>":"<th>add</th><th>edit</th><th>delete</th><th>view</th>"));
 		if ($canAddEditors) {
 			printc("<th>&nbsp;</th>");
 		}
@@ -79,7 +79,7 @@ class permissions {
 					//if ($e == 'everyone') continue;
 					// :: /hack ::
 					
-					printc("<tr><td class=td$color align=left>");
+					printc("\n<tr><td class=td$color align=left>");
 					if ($e == "everyone")
 						printc("Everyone (will override other entries)</td>");
 					else if ($e == "institute")
@@ -93,19 +93,19 @@ class permissions {
 						if (!$d && $i==4) $skip = 1;
 						if (!$d && (($e == 'everyone' || $e == 'institute') && $i!=3)) $nob=1;
 						if (!$skip) {
-							printc("<td class=td$color align=center>");
+							printc("\n<td class=td$color align=center>");
 							if ($nob) printc("&nbsp;");
-							else printc("<input type=checkbox name='permissions[$e][$i]' value=1".(($permissions[$e][$i])?" checked":"").">");
+							else printc("\n<input type=checkbox name='permissions[$e][$i]' value=1".(($permissions[$e][$i])?" checked":"").">");
 							printc("</td>");
 						}
 						if ($skip || $nob) {
-							printc("<input type=hidden name='permissions[$e][$i]' value=".$permissions[$e][$i].">");
+							printc("\n<input type=hidden name='permissions[$e][$i]' value=".$permissions[$e][$i].">");
 						}
 					}
 					
 					printc("</td>");
 					if ($canAddEditors) {
-						printc("<td class=td$color align=center>");
+						printc("\n<td class=td$color align=center>");
 						if ($e == 'everyone' || $e == 'institute') printc("&nbsp;");
 						else printc("<a href='#' onClick='delEditor(\"$e\");'>remove</a>");
 						printc("</td>");
@@ -115,13 +115,13 @@ class permissions {
 				}
 				
 			}
-		} else printc("<tr><td class=td1 > &nbsp; </td><td class=td1 colspan=".($a[$d]+1).">no editors added</td></tr>");
+		} else printc("\n<tr><td class=td1 > &nbsp; </td><td class=td1 colspan=".($a[$d]+1).">no editors added</td></tr>");
 		
 		if ($canAddEditors) {
-			printc("<tr><th colspan=".($a[$d]+1).">".((isclass($sitename) && !in_array($sitename,$edlist))?"<a href='#' onClick='addClassEditor();'>Add students in ".$sitename."</a>":"&nbsp;")."</th><th><a href='add_editor.php?$sid' target='addeditor' onClick='doWindow(\"addeditor\",400,250);'>add editor</a></th></tr>");
+			printc("\n<tr><th colspan=".($a[$d]+1).">".((isclass($sitename) && !in_array($sitename,$edlist))?"<a href='#' onClick='addClassEditor();'>Add students in ".$sitename."</a>":"&nbsp;")."</th><th><a href='add_editor.php?$sid' target='addeditor' onClick='doWindow(\"addeditor\",400,250);'>add editor</a></th></tr>");
 		}
 		
-		printc("</table>");
+		printc("\n</table>");
 		
 //		if ($_SESSION[settings][edit]) printc("<a href='editor_access.php?$sid&site=".$sitename."' onClick='doWindow(\"permissions\",600,400)' target='permissions'>Permissions as of last save</a>");
 		
