d09a480da6abf86030a1d03a5d2cfe4c7167ab7a
Adam Franco <afranco@middlebury.edu>
2004-09-10 15:39:07 +0000

    Changing auser as admin now keeps you on the same page.

--- change_auser.inc.php
+++ change_auser.inc.php
@@ -2,38 +2,16 @@
 // changes the active user
 //include("dbwrapper.inc.php");
 $changeauser = strtolower($_REQUEST[changeauser]);
-print "change_auser started with $changeauser...<BR>";
-$debug = 1;
+//print "change_auser started with $changeauser...<BR>";
+$debug = 0;
 if ($ltype == 'admin') {	// must be admin to do this:
-	print "we are admin.";
+//	print "we are admin.";
 	//printpre($_auth_mods);
-
-// we'll check first the db, to see if they're there, then ldap if they're not there.
-/* 	if ($r=db_valid($changeauser,"",1)) { */
-/* 		$a = db_fetch_assoc($r); */
-/* 		$auser = $changeauser; */
-/* 		$aemail = $a['email']; */
-/* 		$afname = $a['fname']; */
-/* 		$atype = $a['type']; */
-/* 		$aid = $a['id']; */
-/* 		$amethod = 'db'; */
-/* 		 */
-/* 		log_entry("change_auser","","","","$luser as $auser"); */
-/* 	} else if ($res = ldap_valid($changeauser,"",1)) { */
-/* 		$aid = db_get_value("users","id","uname='$changeauser'"); */
-/* 		$afname = db_get_value("users","fname","uname='$changeauser'"); */
-/* 		$aemail = db_get_value("users","email","uname='$changeauser'"); */
-/* 		$atype = db_get_value("users","type","uname='$changeauser'"); */
-/* 		$auser = $changeauser; */
-/* 		$amethod = 'ldap'; */
-/* 		 */
-/* 		log_entry("change_auser","","","","$luser as $auser"); */
-/* 	} */
 
 	$valid = 0;
 	foreach ($_auth_mods as $_auth) {
 		$func = "_valid_".$_auth;
-			print "<BR>AUTH: trying ".$_auth ."..."; //debug
+//			print "<BR>AUTH: trying ".$_auth ."..."; //debug
 		if ($x = $func($changeauser,"",1)) {
 			$valid = 1;
 			break;
@@ -52,9 +30,13 @@ if ($ltype == 'admin') {	// must be admin to do this:
 	}
 }
 
-printpre("<p>$aid, $afname, $auser, $aemail, $atype<BR>");
+//printpre("<p>$aid, $afname, $auser, $aemail, $atype<BR>");
 //exit();
-header("Location: index.php?$sid");
+$getVars = "";
+foreach ($_GET as $key => $val) {
+		$getVars .= "&".$key."=".$val;
+	}
+header("Location: index.php?$sid".$getVars);
 
 ?>
 	
