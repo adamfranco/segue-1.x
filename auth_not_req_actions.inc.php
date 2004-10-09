8f067ec689d6e64ef9083b397de93af4af929dcb
Adam Franco <afranco@middlebury.edu>
2004-10-09 03:52:48 +0000

    adding rss as a not required action.

--- auth_not_req_actions.inc.php
+++ auth_not_req_actions.inc.php
@@ -3,4 +3,17 @@
 // List of no_auth actions:
 
 // this array contains a list of actions that don't *require* the user to be authenticated
-$actions_noauth = array("site","login","default","previewtheme","fullstory","fullstory.php","list","username_lookup","listarticles","listissues","sitelisting.php");
+$actions_noauth = array(
+	"site",
+	"login",
+	"default",
+	"previewtheme",
+	"fullstory",
+	"fullstory.php",
+	"list",
+	"username_lookup",
+	"listarticles",
+	"listissues",
+	"sitelisting.php",
+	"rss"
+);
