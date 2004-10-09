468cf698ad30ad28ea8a217bab84f57cee9e9882
Adam Franco <afranco@middlebury.edu>
2004-10-09 06:45:08 +0000

    Getting cross-site authentication working

--- output_modules/other/rss.inc.php
+++ output_modules/other/rss.inc.php
@@ -24,7 +24,6 @@ if (ereg("^".$cfg[full_uri], $url)
 	$replacement = "index.php?".session_name()."=".session_id();
 	$url = ereg_replace("index.php\?", $replacement, $url);
 	print "\n$url\n<br />";
-	exit;
 }
 
 // If we have an auser, create a cache just for them.
