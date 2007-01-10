9588ac27a9d412d9398fa6d6b8a8dfad80858f09
Adam Franco <afranco@middlebury.edu>
2007-01-10 17:51:14 +0000

    Adding help

---
+++ help/topics/customcss.inc.php
@@ -0,0 +1,8 @@
+<? $title = "Custom CSS"; ?>
+<p><strong>Cascading Style Sheets (CSS)</strong> is a stylesheet language used to describe the presentation of a document written in a markup language such as HTML. (<a href='http://en.wikipedia.org/wiki/Cascading_Style_Sheets'>more at Wikipedia...</a>)</p>
+
+<p><strong>CSS references:</strong>
+<ul>
+	<li><a href='http://www.w3schools.com/css/css_syntax.asp'>W3 Schools' CSS Tutorial</a>
+	<li><a href='http://www.blooberry.com/indexdot/css/index.html'>Index DOT CSS</a> - A good refrence of CSS properties and selectors.</li>
+</ul>
