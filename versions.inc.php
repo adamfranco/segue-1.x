849146cae6b2ccd950c26245b03c574d6ca6f275
Adam Franco <afranco@middlebury.edu>
2007-02-02 18:06:41 +0000

    Javascript radio selection now working.

--- versions.inc.php
+++ versions.inc.php
@@ -46,34 +46,25 @@ if ($storyObj->getField("type") == 'file') {
 /******************************************************************************
  * print out shory and discussion (if any)
  ******************************************************************************/
-// require_once("htmleditor/editor.inc.php");
-if ($storyObj->getField("discuss")) $titleExtra = " Discussion";
 
 printc("\n<table width='100%' id='maintable' cellspacing='1'>");
 
 printc($pagePagination);
-if ($_REQUEST['selversions']) {
-	$_SESSION['selversions'] = $_REQUEST['selversions'];
-}
-
-
-//printpre($_SESSION['selversions']);
-//printpre($_REQUEST['selversions']);
 
 /******************************************************************************
  * print out title and options
  ******************************************************************************/
  
-printc("<tr><td align='left' class=title>");
-printc("<a href=index.php?action=site&".$getinfo2.">".spchars($pageObj->getField('title'))."</a>");
+printc("<tr><td align='left' class='title'>");
+printc("<a href='index.php?action=site&amp;".$getinfo2."'>".spchars($pageObj->getField('title'))."</a>");
 
 if ($storyObj->getField('title')) {
 	printc(" > ".spchars($storyObj->getField('title')));
 }
 
-if ($_REQUEST['selversions']) {
-	$_SESSION['selversions'] = $_REQUEST['selversions'];
-	//printpre($_SESSION['selversions']);
+if ($_REQUEST['oldversion'] && $_REQUEST['newversion']) {
+	$_SESSION['oldversion'] = $_REQUEST['oldversion'];
+	$_SESSION['newversion'] = $_REQUEST['newversion'];
 	printc(" > <a href='index.php?$sid&amp;action=site&amp;site=$site&amp;section=$section&amp;page=$page&amp;story=$story&amp;versioning=$story'>All Versions</a>");
 	printc(" > Selected Versions");
 	
@@ -105,11 +96,11 @@ printc("</td></tr>\n");
  * if selected versions request, then print out selected versions to compare
  ******************************************************************************/
 	
-if ($_REQUEST['selversions']) {
+if ($_REQUEST['oldversion'] && $_REQUEST['newversion']) {
 
-	$version01 = get_versions($story, $_REQUEST['selversions'][0]);
+	$version01 = get_versions($story, $_REQUEST['oldversion']);
 	$version01_num = $version01[0]['version_order'];
-	$version02 = get_versions($story, $_REQUEST['selversions'][1]);
+	$version02 = get_versions($story, $_REQUEST['newversion']);
 	$version02_num = $version02[0]['version_order'];
 //	printpre($version01);
 	//printpre($version02);	
@@ -141,11 +132,74 @@ if ($_REQUEST['selversions']) {
  ******************************************************************************/
 
 } else if ($_REQUEST['versioning']) {
-	$u = "$PHP_SELF?$sid&amp;action=site&site=$site&section=$section&page=$page&story=$story&versioning=1";
-	printc("<form action=$u method='post'>");
-	printc("<tr><td>");
+	printc("\n<tr>\n\t<td>\n\t\t");
+	$u = "$PHP_SELF?$sid&amp;action=site&amp;site=$site&amp;section=$section&amp;page=$page&amp;story=$story&amp;versioning=1";
+	printc("<form action='$u' method='post'>\n");
+	
+	ob_start();
+	
+	print<<<END
+	
+	<script type='text/javascript'>
+	// <![CDATA[
+	
+	/**
+	 * Update the radio buttons checked if needed to preven un-allowed situations:
+	 * 		- comparing a version to itself
+	 *		- mixing up new and old versions
+	 * 
+	 * @param object RadioButton button
+	 * @return void
+	 * @access public
+	 * @since 2/2/07
+	 */
+	function updateVersionSelection (button) {
+		var oldButtons = new Array();
+		oldButtons.push(null);
+		var newButtons = new Array();
+		
+		for (var i = 0; i < button.form.elements.length; i++) {
+			// Sort the elements into old and new arrays and record
+			// what row in each is selected
+			var element = button.form.elements[i];
+			if (element.name == 'oldversion') {
+				oldButtons.push(element);
+				if (element.checked)
+					var oldRow = oldButtons.length - 1;
+			} else if (element.name == 'newversion') {
+				newButtons.push(element);
+				if (element.checked)
+					var newRow = newButtons.length - 1;
+			}
+		}
+		
+		newButtons.push(null);
+				
+		// If a new version was selected make sure that the old version is older
+		if (button.name == 'newversion') {
+			if (oldRow <= newRow) {
+				oldButtons[oldRow].checked = '';
+				oldButtons[newRow + 1].checked = 'checked';
+			}
+		} 
+		// If an old version was selected make sure that the new version is newer
+		else {
+			if (newRow >= oldRow) {
+				newButtons[newRow].checked = '';
+				newButtons[oldRow - 1].checked = 'checked';
+			}
+		}
+	}
+	
+	// ]]>
+	</script>
+	
+	
+END;
+	printc(ob_get_clean());
+	
 	// compare selected versions button (top)
-	printc("<br \><button type='submit' class='button' value='compare' onClick=\"window.location='$u'\">Compare selected revisions</button><br /><br />");
+	printc("<br /><button type='submit' class='button' value='compare' onclick=\"window.location='$u'\">Compare selected revisions</button><br /><br />");
 	printc("<table cellspacing='3' width='100%'>\n");
 	$versions = get_versions($storyObj->id);
 	//printpre($versions);	
@@ -159,7 +213,7 @@ if ($_REQUEST['selversions']) {
 		$version_num = $version['version_order'];
 		
 		printc("<tr>\n");
-		printc("<td class=ts$color align='right'>");
+		printc("<td class='ts$color' align='right'>");
 		
 		if ($i > 0) {
 			printc("<input type='radio' name='oldversion' value='".$version_num."' ");
@@ -172,10 +226,12 @@ if ($_REQUEST['selversions']) {
 					printc(" checked='checked'");
 			}
 			
+			printc(" onclick=\"updateVersionSelection(this);\"");
+			
 			printc(" />");
 		}
 		
-		printc("</td>\n<td class=ts$color align='left'>");
+		printc("</td>\n<td class='ts$color' align='left'>");
 		
 		if ($i < count($versions) - 1) {
 			printc("<input type='radio' name='newversion' value='".$version_num."' ");
@@ -188,13 +244,14 @@ if ($_REQUEST['selversions']) {
 					printc(" checked='checked'");
 			}
 			
+			printc(" onclick=\"updateVersionSelection(this);\"");
 			printc(" />");
 		}
 
 		printc("</td>");
-		printc("<td class=ts$color><a href='index.php?$sid&amp;action=site&amp;site=$site&amp;section=$section&amp;page=$page&amp;story=$story&amp;version=$version_num'>Revision $version_num</a></td>");
-		printc("<td class=ts$color>".$version['version_created_tstamp']."</td>");
-		printc("<td class=ts$color>".$version['FK_createdby']."</td>\n");
+		printc("<td class='ts$color'><a href='index.php?$sid&amp;action=site&amp;site=$site&amp;section=$section&amp;page=$page&amp;story=$story&amp;version=$version_num'>Revision $version_num</a></td>");
+		printc("<td class='ts$color'>".$version['version_created_tstamp']."</td>");
+		printc("<td class='ts$color'>".$version['FK_createdby']."</td>\n");
 		printc("</tr>\n");
 		
 		$color = 1-$color;
@@ -204,8 +261,8 @@ if ($_REQUEST['selversions']) {
 	printc("</table>\n");
 	// compare selected versions button (bottom)
 	printc("<br /><button type='submit' class='button' value='compare'>Compare selected revisions</button><br /><br /> ");
-	printc("</form>");
-	printc("</td></tr>");
+	printc("\n\t\t</form>");
+	printc("\n\t</td>\n</tr>");
 
 
 /******************************************************************************
@@ -221,12 +278,12 @@ if ($_REQUEST['selversions']) {
 	// Revert to this version link (top location)
 	if ($_REQUEST['version']  && $storyObj->hasPermission("edit")) {
 		printc("<tr><td>");
-		printc("<br \><table width='100%' cellspacing='0'><tr><td>");
+		printc("<br /><table width='100%' cellspacing='0'><tr><td>");
 		printc("<strong>Revision ".$version_num."</strong> (".$version_date." - ".$version_author.")");
 		printc("</td><td align='right'>");
 		// revert to this version link (top)
 		printc("<a class='btnlink2' href='index.php?$sid&amp;action=edit_story&amp;site=$site&amp;section=$section&amp;page=$page&amp;edit_story=$story&amp;version=$version_num&amp;comingFrom=viewsite'>Revert to this Version</a>\n");
-		printc("</td></td></table><br \>");
+		printc("</td></td></table><br />");
 		printc("</td></tr>\n");
 		printc("<tr><td width='100%' valign='top' style='border: 1px dotted #CCCCCC;'>$smalltext</td></tr>\n");
 		printc("<tr><td width='100%' valign='top' style='border: 1px dotted #CCCCCC;'>$fulltext</td></tr>\n");
@@ -263,8 +320,8 @@ if ($_REQUEST['selversions']) {
 		
 		// Revert to this version link (bottom location)
 		if ($_REQUEST['version']  && $storyObj->hasPermission("edit")) {
-			printc("<tr><td align='center'><br \>");
-			printc("<a class='btnlink2' href='index.php?$sid&amp;action=edit_story&amp;site=$site&amp;section=$section&amp;page=$page&amp;edit_story=$story&amp;version=$version_num&amp;comingFrom=viewsite'>Revert to this Version</a><br \><br \>\n");
+			printc("<tr><td align='center'><br />");
+			printc("<a class='btnlink2' href='index.php?$sid&amp;action=edit_story&amp;site=$site&amp;section=$section&amp;page=$page&amp;edit_story=$story&amp;version=$version_num&amp;comingFrom=viewsite'>Revert to this Version</a><br /><br />\n");
 			printc("</td></tr>\n");
 		}
 	
@@ -278,15 +335,14 @@ if ($_REQUEST['selversions']) {
  * print out title and options
  ******************************************************************************/
  
-printc("<tr><td align='left'");
-printc("<a href=index.php?action=site&".$getinfo2.">".spchars($pageObj->getField('title'))."</a>");
+printc("<tr><td align='left'>");
+printc("<a href='index.php?action=site&amp;".$getinfo2."'>".spchars($pageObj->getField('title'))."</a>");
 
 if ($storyObj->getField('title')) {
 	printc(" > ".spchars($storyObj->getField('title')));
 }
 
-if ($_REQUEST['selversions']) {
-	//printpre($_SESSION['selversions']);
+if ($_REQUEST['oldversion'] && $_REQUEST['newversion']) {
 	printc(" > <a href='index.php?$sid&amp;action=site&amp;site=$site&amp;section=$section&amp;page=$page&amp;story=$story&amp;versioning=$story'>All Versions</a>");
 	printc(" > Selected Versions");
 	
@@ -306,9 +362,6 @@ if ($_REQUEST['selversions']) {
 }
 printc("</td></tr>\n");
 
-
-		printc("<table>");
-printc("\n\t<tr>\n\t\t<td align='left'>\n\t\t\t<br /><a href='index.php?action=site&amp;".$getinfo2."'>".spchars($pageObj->getField('title'))."</a> &gt; in depth</td>\n\t</tr>");
 printc("</table>\n");
 
 
