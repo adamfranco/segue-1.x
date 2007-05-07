10836d446749511c7535aa5d6428a73a0ba50f1b
Adam Franco <afranco@middlebury.edu>
2007-05-07 16:19:56 +0000

    Updating changelog parser.

--- changelog/generate.sh
+++ changelog/generate.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#  @package concerto.docs
+#  
+#  @copyright Copyright &copy; 2005, Middlebury College
+#  @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+#  
+#  @version $Id$
+
+progdir=`dirname $0`
+cd $progdir
+
 xsltproc ../xslt/changelog-simplehtml.xsl changelog.xml | sed -e 's/<?xml.*?>//' > changelog.html
 xsltproc ../xslt/changelog-plaintext.xsl changelog.xml | sed -e 's/<?xml.*?>//' > changelog.txt
 
