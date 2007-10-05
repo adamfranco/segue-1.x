80781d798049e7798ca7e5075cd99aed1ea07daf
Adam Franco <afranco@middlebury.edu>
2007-10-05 20:51:04 +0000

    Delete Site now works.

--- delete_site.inc.php
+++ delete_site.inc.php
@@ -2,25 +2,25 @@
 
 $pagetitle = "Delete Site";
 
-$s =& new site($_REQUEST[name]);
+$s =& new site($_REQUEST['name']);
 $s->fetchDown(1);
 /* print "<pre>"; print_r($s); print "</pre>"; */
 
-if (sitenamevalid($_REQUEST[name])) {
+if (sitenamevalid($_REQUEST['name'])) {
 	if ($_REQUEST[confirm]) {
 		$s->delete();
-		log_entry("delete_site","$_SESSION[auser] deleted site $_REQUEST[name]",$s->getField("name"),$s->id,"site");
+		log_entry("delete_site",$_SESSION['auser']." deleted site ".$_REQUEST['name'],$s->getField("name"),$s->id,"site");
 	} else {
 		printc("Are you <b>SURE</b> you want to delete the site <i>".$s->getField("title")."</i>?? This operation is <b>irreversable</b>. You will <b>never</b> see any of the content of this site again, including all sections, pages, content, and discussions. You better be <b>ABSOLUTELY SURE</b> you want to do this! If so, hit 'Delete'.<br /><br />");
-		printc("<form action='$PHP_SELF?$sid&amp;action=delete_site&amp;name=$_REQUEST[name]'><input type='hidden' name='confirm' value='1' />");
-		printc("<input type='hidden' name='name' value='$name' />");
+		printc("<form action='$PHP_SELF?$sid&amp;action=delete_site&amp;name=".$_REQUEST['name']."'><input type='hidden' name='confirm' value='1' />");
+		printc("<input type='hidden' name='name' value='".$_REQUEST['name']."' />");
 		printc("<input type='hidden' name='action' value='delete_site' />");
 		printc("<input type='button' value='&lt;&lt; Back' onclick='history.go(-1)' /> <input type='submit' value='Delete' />");
 		printc("</form>");
 /* 		print "<pre>"; print_r($s); print "</pre>"; */
 		return;
 	}
-} else log_entry("delete_site","$_SESSION[auser] deleting site $_REQUEST[name] failed",$s->getField("name"),$s->id,"site");
+} else log_entry("delete_site", $_SESSION['auser']." deleting site '".$_REQUEST['name']."' failed",$s->getField("name"),$s->id,"site");
 
 header("Location: $PHP_SELF?$sid");
 exit;
